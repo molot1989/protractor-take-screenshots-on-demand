044bf320637bacbf22660ccff5cc10be1bad6b10
roman <roman@riflmedia.com>
2016-04-22 19:54:56 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+
+module.exports  = {
+
+    screenShotDirectory:  '',
+
+    createDirectory: function () {
+        mkdirp(this.screenShotDirectory, function(err) {
+            if(err) {
+                console.log('Could not create directory ' + this.screenShotDirectory);
+            }
+        });
+    },
+
+    takeScreenshot: function (name) {
+        var directory = this.screenShotDirectory;
+        function writeScreenShot (data, filename) {
+            fs.existsSync(directory);
+            var stream = fs.createWriteStream(directory + '/' + filename);
+            stream.write(new Buffer(data, 'base64'));
+            stream.end();
+            console.log(a)
+        }
+        browser.takeScreenshot().then(function (png) {
+            browser.getCapabilities().then(function (capabilities) {
+                var browserName = capabilities.caps_.browserName,
+                    filename = browserName + ' - ' + name + '.png';
+                writeScreenShot(png, filename);
+            });
+        });
+    }
+
+};
